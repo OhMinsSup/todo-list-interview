f4381e671c1962090cb94aac10a612a6291411b4
OhMinSeop <mins5190@gmail.com>
2025-02-16 01:50:44 +0900

    chore: prettier 필요없는 기능 제거

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,15 +1,11 @@
-import { fileURLToPath } from "node:url";
-
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
-/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
   plugins: [
     "@ianvs/prettier-plugin-sort-imports",
     "prettier-plugin-packagejson",
-    "prettier-plugin-tailwindcss",
   ],
   tailwindFunctions: ["cn", "cva"],
   importOrder: [
